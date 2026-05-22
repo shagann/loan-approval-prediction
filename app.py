a0f3b68d66cf653f941cc7088d00f9916d06943d
shagann <shgannon@gmail.com>
2026-05-22 12:41:48 +0100

    Add health and version endpoints

--- app.py
+++ app.py
@@ -354,4 +354,4 @@ def results():
                          confidence=confidence)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
